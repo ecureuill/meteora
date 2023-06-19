36b983a2334a7edcd3f6e093b7e7585c67393bb3
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-19 19:21:08 -0300

    📱(MediaQuery) update

--- src/hooks/useMediaQueries.tsx
+++ src/hooks/useMediaQueries.tsx
@@ -3,8 +3,8 @@ import { useMediaQuery as umq } from 'react-responsive';
 const useMediaQuery = (query: string): boolean => umq({ query });
 
 const useMediaQueries = () => {
-	const isTinyVW = useMediaQuery('(max-device-width: 549px)');
-	const isSmallVW = useMediaQuery('(min-device-width: 550px) and (max-device-width: 911px)');
+	const isTinyVW = useMediaQuery('(max-device-width: 375px)');
+	const isSmallVW = useMediaQuery('(min-device-width: 376px) and (max-device-width: 911px)');
 	const isMediumVW = useMediaQuery('(min-device-width: 912px) and (max-device-width: 1043px)');
 	const isLargeVW = useMediaQuery('(min-device-width: 1044px)');
 
