66f5be3144b2dbeac7380e601e2a791bfa7cc57d
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-19 11:02:08 -0300

    💄 (Text) create component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,42 @@
+import { HTMLAttributes } from 'react';
+import './styles.css';
+
+interface TextProps extends HTMLAttributes<HTMLElement> {
+	variant?: 'title-1' | 'title-2' | 'paragraph';
+	size?: 'small' | 'medium' | 'large' | 'big';
+	weight?: 'normal' | 'bold' | 'bolder'
+}
+
+const Text = ( {variant, size, children, className, weight='normal', ...props}: React.PropsWithChildren<TextProps>): JSX.Element => {
+	const styles = `Text Text--${weight} ${className? className: ''}`;
+
+	switch(variant){
+	case 'title-1':
+		return (
+			<h1 className={`${styles} Text--heading Text--${size === undefined? 'big': size}`} {...props}>
+				{children}
+			</h1>
+		);
+	case 'title-2':
+		return (
+			<h2 className={`${styles} Text--heading Text--${size === undefined? 'large': size}`} {...props}>
+				{children}
+			</h2>
+		);
+
+	case 'paragraph':
+		return(
+			<p className={`${styles} Text--${size === undefined? 'medium': size}`} {...props}>
+				{children}
+			</p>
+		);
+	default:
+		return(
+			<span className={`${styles} Text--${size === undefined? 'medium': size}`} {...props}>
+				{children}
+			</span>
+		);	
+	}
+};
+
+export default Text;
