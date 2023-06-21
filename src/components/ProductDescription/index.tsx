5d0c57cecad90499bef5a05f9adcd2e812ef40d0
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2023-06-20 22:50:38 -0300

    💄 (ProductSection | ProductDescription) update component

--- src/components/ProductDescription/index.tsx
+++ src/components/ProductDescription/index.tsx
@@ -1,25 +1,56 @@
-import { Button, Text } from '../.';
+import { useEffect, useState } from 'react';
+import { Button, Modal, ProductDetails, Text } from '../.';
 import data from '../../l10n/pt-BR.json';
+import { products } from '../../services/products';
 import './styles.css';
 
 type ProductDescriptionProps = {
-	title: string, 
-	description: string,
-	price: string,
+	productId: string, 
 };
 
-const ProductDescription = ( {title, description, price}: ProductDescriptionProps): JSX.Element => {
+const ProductDescription = ( {productId}: ProductDescriptionProps): JSX.Element => {
+
+	const [ openModal, setOpenModal ] = useState(false);
+	const [ product, setProduct] = useState<null|any>(null);
+
+	useEffect(() => {
+		const prod = products.find(prod => prod.id === productId);
+
+		if(prod !== undefined)
+			setProduct(prod);
+
+	}, []);
+
+	if(product === null)
+		return <>carregando...</>;
 
 	return (
 		<>
-			<Text variant='title-3' size='medium' weight='bolder'>{title}</Text>
+			<Text variant='title-3' size='medium' weight='bolder'>{product.title}</Text>
 			<div className='ProductDescription__content'>
 				<Text variant='paragraph'>
-					{description}
+					{product.description}
 				</Text>
 			</div>
-			<Text variant='paragraph' weight='bolder'><strong>{price}</strong></Text>
-			<Button>{data['controls.button.moredetails.label']}</Button>
+			<Text variant='paragraph' weight='bolder'><strong>{product.price}</strong></Text>
+			<Button onClick={() => setOpenModal(true)}>{data['controls.button.moredetails.label']}</Button>
+			<Modal 
+				open={openModal}
+				title={data['controls.modal.products.title']}
+				className='Modal--border'
+			>
+
+				<ProductDetails
+					colors={product.colors} 
+					img={product.img} 
+					title={product.title} 
+					description={product.description}
+					price={product.price} 
+					sizes={product.sizes} 
+					sellBy={product.sellBy} 
+					deliveredBy={product.deliveredBy}
+				/>
+			</Modal>	
 		</>
 	);
 };
